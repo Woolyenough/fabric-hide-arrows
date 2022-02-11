56c9b4b5b8af47423c3c268c5b4c0cfaf88aaf3c
Woolyenough <99221540+Woolyenough@users.noreply.github.com>
2022-02-11 23:54:50 +0000

    Add files via upload

---
+++ src/main/java/net/woolyenough/hidearrows/HideArrows.java
@@ -0,0 +1,14 @@
+package net.woolyenough.hidearrows;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.api.ModInitializer;
+
+@Environment(EnvType.CLIENT)
+public final class HideArrows implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
